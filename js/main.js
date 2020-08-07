6298046f04e4c6772be0f12b287514805afff474
Marina <67875923+Marina51533@users.noreply.github.com>
2020-08-07 23:43:38 +0200

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 0,
+        autoplay: true,
+        autoplayTimeout: 14000,
+        autoplaySpeed: 6000,
+        autoplayHoverPause: true,
+        lazyLoad: true,
+
+    });
+
 });
